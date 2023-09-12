99dc64508c53be97f16a0eff0e0923468650a157
Jason Liu <jason.liu29@outlook.com>
2023-09-12 13:36:30 -0700

    upgrade priroity of static process

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Game Prioritizer
 
 Increase game perforance and stabilize fps by automatically increasing the Windows priority of the specific processes. This will help keep the negative or slowign effect of background apps and prosseces to a minimal amount and make games run better.
+
+## Who is this for?
+Someone who is ready to break their monitor over jittery gameplay
+You'll find the most use out of this fix if:
+1. You experience significant fps fluctuations while gaming
+1. The range of these fluctuations often go above and below your monitor refresh rate
+
+## How do I use it
+tbd - still a work in progress
--- main.py
+++ main.py
@@ -2,15 +2,30 @@ import psutil
 import time;
 
 target_processes = {
-    'valorant'
+    'VALORANT-Win64-Shipping.exe'
 }
-
-def main():
-    # for process in psutil.process_iter(attrs=['pid', 'name']):
+            
+# find the target processes if they are running and return their pid
+def find_process():
+    output = []
     for process in psutil.process_iter(['pid', 'name']):
         print('pid:' + str(process.info['pid']) + ' | name: ' + process.info['name'])
-        if process.info['name'] in target_processes
-            
-        
+        if process.info['name'] in target_processes:
+            output.append(process)
+    return(output)
+
+# increase the priority of the process running with pid parameter
+def upgrade_priority(pid):
+    target_process = psutil.Process(pid)
+    target_process.nice(psutil.HIGH_PRIORITY_CLASS)
+    
+# reallocate the cores that handle the process such that they are not handled by cpu0
+#def reallocate_process(pid):
+
+def main():
+    targets = find_process()
+    print('found' + str(targets))
+    for process in targets:
+        upgrade_priority(process.info['pid'])
 
 main()
