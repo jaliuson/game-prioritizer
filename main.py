5d393e28e108cba43d65ef0838bab3a18191abb5
Jason Liu <jason.liu29@outlook.com>
2023-09-19 03:13:29 -0700

    added affinity optimzation

--- main.py
+++ main.py
@@ -2,30 +2,40 @@ import psutil
 import time;
 
 target_processes = {
-    'VALORANT-Win64-Shipping.exe'
+    'VALORANT-Win64-Shipping.exe',
+    'League of Legends.exe'
 }
             
 # find the target processes if they are running and return their pid
 def find_process():
     output = []
     for process in psutil.process_iter(['pid', 'name']):
-        print('pid:' + str(process.info['pid']) + ' | name: ' + process.info['name'])
+        #print('pid:' + str(process.info['pid']) + ' | name: ' + process.info['name'])
         if process.info['name'] in target_processes:
             output.append(process)
+    print(f"Found \t\t {len(psutil.pids())} \tprocesses (total)")
+    print(f'Optimizing \t {len(output)} \tprocesses (target)')
     return(output)
 
 # increase the priority of the process running with pid parameter
 def upgrade_priority(pid):
     target_process = psutil.Process(pid)
     target_process.nice(psutil.HIGH_PRIORITY_CLASS)
+    print('switched ' + target_process.info['name'] + ' to HIGH priority')
     
 # reallocate the cores that handle the process such that they are not handled by cpu0
-#def reallocate_process(pid):
+def reallocate_process(pid):
+    core_count = psutil.cpu_count()
+    target_process = psutil.Process(pid)
+    target_process.cpu_affinity(range(1,core_count))
+    print('switched ' + target_process.info['name'] + ' affinity to exclude CPU0')
+
 
 def main():
     targets = find_process()
-    print('found' + str(targets))
+    # print('found' + str(targets))
     for process in targets:
         upgrade_priority(process.info['pid'])
+        reallocate_process(process.info['pid'])
 
 main()
